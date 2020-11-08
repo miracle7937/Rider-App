9892273c463e4d86a7a1f9548abbc07506329fed
ebuka.miracle <DF_Ebuka.Miracle@zenithbank.com>
2020-11-08 02:11:55 +0100

    production 1 keys

--- lib/static_content/API_KEY.dart
+++ lib/static_content/API_KEY.dart
@@ -1,3 +1,3 @@
-var kGoogleApiKey = 'AIzaSyC3k-0cLxoOk_BKDQeJyLPrVF_2k3sAMP';
+var kGoogleApiKey = 'AIzaSyC3k-0cLxoOk_BKDQeJyLPrVF_2k3sAMPE';
 var payStackKey = 'pk_live_c98a0c24f90f25a4c3b76a922c97e3a2fadc00ca';
 var onSignalKey = "e47d7647-843f-4465-a63a-1b0f285c9534";
